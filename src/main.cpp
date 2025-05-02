175cec01ca49dac15bcd33b6393525017c6064d8
Kshitij Aucharmal <kshitijaucharmal21@gmail.com>
2025-05-02 12:30:00 +0530

    Camera Movement Done, very bad code

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,8 @@
 // Imgui Based UI
 #include "gui.hpp"
 
+#include "camera.cpp"
+
 // Cube positions
 glm::vec3 cubePositions[] = {
     glm::vec3( 0.0f,  0.0f,  0.0f),
@@ -38,13 +40,50 @@ static const int HEIGHT = 600;
 static const float ASPECTRATIO = (float)WIDTH / (float)HEIGHT;
 static const std::string WINDOWNAME = "ConceptForge";
 
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = WIDTH / 2.0f;
+float lastY = HEIGHT / 2.0f;
+bool firstMouse = true;
+
 static glm::vec4 clearColor(0.05, 0.05, 0.05, 1.0f);
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+  camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
+
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+  float xpos = static_cast<float>(xposIn);
+  float ypos = static_cast<float>(yposIn);
+
+  if (firstMouse)
+  {
+    lastX = xpos;
+    lastY = ypos;
+    firstMouse = false;
+  }
+
+  float xoffset = xpos - lastX;
+  float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+  lastX = xpos;
+  lastY = ypos;
+
+  camera.ProcessMouseMovement(xoffset, yoffset);
+}
 
 int main() {
   std::cout << ASPECTRATIO << std::endl;
   WindowManagement::Window window(WIDTH, HEIGHT, WINDOWNAME, false);
   InputManagement::Input input;
   ShaderManagement::ShaderProgram shaderProgram(DrawMode::FILLED);
+
+  // Capture Mouse
+  glfwSetInputMode(window.window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+  glfwSetCursorPosCallback(window.window, mouse_callback);
+  glfwSetScrollCallback(window.window, scroll_callback);
 
   // Setup Dear ImGui context
   IMGUI_CHECKVERSION();
@@ -59,43 +98,57 @@ int main() {
   shaderProgram.LinkShaders();
   shaderProgram.BindTextures();
 
+  float deltaTime = 0.0f;	// Time between current frame and last frame
+  float lastFrame = 0.0f; // Time of last frame
+  const float cameraSpeed = 5.0f; // adjust accordingly
+
+  glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
+  glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+  glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
   // Matrix transformations for projecting to the screen
   // V(clip) = M(projection)⋅M(view)⋅M(model)⋅V(local)
 
-  // 1. Model Matrix
-  glm::mat4 model = glm::mat4(1.0f);
-  model = glm::rotate(model, glm::radians(-45.0f), glm::vec3(1.0f, 1.0f, 0.0f));
-
-  // 2. View Matrix
-  // > To move a camera backwards, is the same as moving the entire scene forward.
-  glm::mat4 view = glm::mat4(1.0f);
-  // ^ translating the scene in the reverse direction of where we want to move
-  view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-
-  // 3. Projection Matrix
-  bool perspective = true;
-  glm::mat4 projection;
-  if(perspective) projection = glm::perspective(glm::radians(45.0f), ASPECTRATIO, 0.1f, 100.0f);
-  else projection = glm::ortho(0.0f, (float)WIDTH, 0.0f, (float)HEIGHT, 0.1f, 100.0f);
 
   // Render Loop
   while (!glfwWindowShouldClose(window.window)) {
+
+    float currentFrame = glfwGetTime();
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
+
     // input processing
     input.processInput(window.window);
+    if (glfwGetKey(window.window, GLFW_KEY_W) == GLFW_PRESS)
+      camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window.window, GLFW_KEY_S) == GLFW_PRESS)
+      camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window.window, GLFW_KEY_A) == GLFW_PRESS)
+      camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window.window, GLFW_KEY_D) == GLFW_PRESS)
+      camera.ProcessKeyboard(RIGHT, deltaTime);
 
     // rendering commands
     // Clear Screen with this color
     glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // also clear the depth buffer now!
 
+    // 1. Model Matrix
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::rotate(model, glm::radians(-45.0f), glm::vec3(1.0f, 1.0f, 0.0f));
+
+    // Camera
+    // Vector from position to origin
+    glm::mat4 view = camera.GetViewMatrix();
+    shaderProgram.setMat4("view", view);
+
+    // 3. Projection Matrix
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), ASPECTRATIO, 0.1f, 100.0f);;
+    shaderProgram.setMat4("projection", projection);
 
     // Send Shader Data
     shaderProgram.SendDataToVS();
     shaderProgram.SendDataToFS();
 
-    // Sending Matrix Transformations to shader
-    shaderProgram.setMat4("view", view);
-    shaderProgram.setMat4("projection", projection);
 
     for(unsigned int i = 0; i < 10; i++)
     {
@@ -109,7 +162,6 @@ int main() {
       shaderProgram.Use();
       shaderProgram.Draw();
     }
-
 
     // check and call events
     glfwPollEvents();
@@ -131,7 +183,6 @@ int main() {
 
     glfwSwapBuffers(window.window);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
   }
 
   return 0;
