666a7e9ddbed123be1c6490121813adf394eeb81
Kshitij Aucharmal <kshitijaucharmal21@gmail.com>
2025-05-21 13:57:48 +0530

    Can Do setup from python, desctructors don't work as intended

--- nanobind/my_ext.cpp
+++ nanobind/my_ext.cpp
@@ -2,7 +2,10 @@
 #include <nanobind/nanobind.h>
 #include <nanobind/stl/string.h>
 
+#include <memory>
+
 #include "setup.hpp"
+using namespace Engine;
 
 namespace nb = nanobind;
 
@@ -10,12 +13,40 @@ float add(float a, float b){
     return a + b;
 }
 
+std::unique_ptr<ConceptForge> forge;
 void Init(){
-    std::cout << "Hello World" << std::endl;
+    forge = std::make_unique<ConceptForge>();
+    std::cout << "Initialised" << std::endl;
+}
+
+bool WindowShouldClose(){
+    return forge->WindowShouldClose();
+}
+
+void CalcDeltaTime(){
+    forge->DeltaTimeCalc();
+}
+void ProcessInput(){
+    forge->ProcessInput();
+}
+void Render(){
+    forge->Render();
+}
+void CalcProjection(){
+    forge->CalcProjection();
+}
+void GUIManagement(){
+    forge->GUIManagement();
 }
 
 NB_MODULE(concept_forge, m) {
     m.def("add", &add);
     m.def("init", &Init);
+    m.def("window_should_close", &WindowShouldClose);
+    m.def("calc_delta_time", &CalcDeltaTime);
+    m.def("process_input", &ProcessInput);
+    m.def("render", &Render);
+    m.def("calc_projection", &CalcProjection);
+    m.def("gui_management", &GUIManagement);
     m.doc() = "Class";
 }
