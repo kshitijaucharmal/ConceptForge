2a72ecfda6b280d1f09bc355a660cdfddd1ffd38
Kshitij Aucharmal <kshitijaucharmal21@gmail.com>
2025-08-10 01:41:08 +0530

    Model added to scene, but not visible

--- shaders/unlit.frag
+++ shaders/unlit.frag
@@ -21,4 +21,5 @@ void main() {
 
   // Just combining
   FragColor = diffuseColor + specularColor;
+  FragColor = vec4(1., 0, 0, 1);
 }
--- src/Components/Primitives/Grid.hpp
+++ src/Components/Primitives/Grid.hpp
@@ -4,6 +4,6 @@
 struct Grid {
     int size = 50;             // Number of lines in each direction
     float spacing = 1.0f;      // Distance between lines
-    glm::vec3 color = glm::vec3(0.4f);
-    bool infinite = false;     // Should grid follow camera?
+    glm::vec3 color = glm::vec3(0.5f);
+    bool infinite = true;     // Should grid follow camera?
 };
